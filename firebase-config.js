05a7d09a6f98a8c2ce47aa8e2253267e7bd3a362
Duc Bui Ngoc <shylvaorchid@Ducs-Mac-mini-2.local>
2026-07-18 16:44:34 +0700

    feat: add Firebase configuration and score saving to Firestore

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDuqeoRo_A3VYlxFXJrxo0M1fOIwJUJvHw",
+  authDomain: "ducbao-quiz-app.firebaseapp.com",
+  projectId: "ducbao-quiz-app",
+  storageBucket: "ducbao-quiz-app.firebasestorage.app",
+  messagingSenderId: "84421557668",
+  appId: "1:84421557668:web:b116c71da8f0c1428b5531",
+  measurementId: "G-T0BY9W3N8W"
+};
